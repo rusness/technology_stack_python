5d11dbe1e38e84a6f87a25ec1c08a18ad44535ae
Alex Popov <rusness@mail.ru>
2023-04-25 14:28:06 +0300

    ДЗ 3.1

---
+++ task3_1.py
@@ -0,0 +1,5 @@
+kind_of_pet = input("Вид питомца: ")
+age_of_pet  = input("Возраст питомца: ")
+name_of_pet = input("Кличка питомца: ")
+
+print('Это '+kind_of_pet+' по кличке "'+name_of_pet+'". '+'Возвраст: '+age_of_pet)
