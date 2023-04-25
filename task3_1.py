6a6a6ca57df02ccd006db078637797da36a645fd
Alex Popov <rusness@mail.ru>
2023-04-25 14:52:38 +0300

    rusness@mail.ru

--- task3_1.py
+++ task3_1.py
@@ -2,4 +2,4 @@ kind_of_pet = input("Вид питомца: ")
 age_of_pet  = input("Возраст питомца: ")
 name_of_pet = input("Кличка питомца: ")
 
-print('Это '+kind_of_pet+' по кличке "'+name_of_pet+'". '+'Возвраст: '+age_of_pet)
+print('Это '+kind_of_pet+' по кличке "'+name_of_pet+'". '+'Возраст: '+age_of_pet)
