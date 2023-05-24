5931ae37ce66f2e3cfeebe4c3a99c81f1dc54c7e
Aleksey Popov <rusness@mail.ru>
2023-05-24 09:20:21 +0300

    Update task5_2.py

--- task5_2.py
+++ task5_2.py
@@ -1,23 +1,32 @@
 word = input("Введите слово: ")
 # гласные буквы a, e, i , o, u
+vowels = "aeiou"
 sum_a = 0
 sum_e = 0
 sum_i = 0
 sum_o = 0
 sum_u = 0
+vowels_count = 0
+consonants_count = 0
 
 for i in word:
     letter = i.lower()
-    if letter == 'a':
-        sum_a += 1
-    elif letter == 'e':
-        sum_e += 1
-    elif letter == 'i':
-        sum_i += 1
-    elif letter == 'o':
-        sum_o += 1
-    elif letter == 'u':
-        sum_u += 1
+    if letter in vowels:
+        vowels_count += 1
+        if letter == 'a':
+            sum_a += 1
+        elif letter == 'e':
+            sum_e += 1
+        elif letter == 'i':
+            sum_i += 1
+        elif letter == 'o':
+            sum_o += 1
+        elif letter == 'u':
+            sum_u += 1
+    elif i.isalpha():
+        consonants_count += 1
+
+
 if sum_a > 0:
     print("Количество 'a' -", sum_a)
 else:
@@ -42,3 +51,6 @@ if sum_u > 0:
     print("Количество 'u' -", sum_u)
 else:
     print("Количество 'u' -", sum_u>0)
+
+print(f"Гласных букв: {vowels_count}")
+print(f"Согласных букв: {consonants_count}")
